fb1cac7fb6079d97e8c8dd2c00eeca056ed8c7f2
s-zehra <zehrataqwi@gmail.com>
2025-08-06 21:16:51 +0300

    feat: added field for  job post and removed file uploader

--- src/components/MatchInput.tsx
+++ src/components/MatchInput.tsx
@@ -16,16 +16,14 @@ import { Textarea } from "./ui/textarea";
 const formSchema = z.object({
   cvContent: z
     .string({
-      required_error:
-        "You need to either upload your CV as a valid TXT, DOCX, or PDF file, or paste it as text.",
+      required_error: "Please paste your CV.",
     })
-    .min(
-      100,
-      "Please provide more information in the CV field or upload a valid file."
-    )
+    .min(100, "Please provide more information in the CV field.")
     .max(50000, "CV content is too long"),
   jobDescription: z
-    .string()
+    .string({
+      required_error: "Please paste your Job Description.",
+    })
     .min(100, "Please provide more information in the Job Description field")
     .max(50000, "Job Description is too long"),
 });
@@ -39,76 +37,68 @@ const MatchInput = () => {
     console.log(data);
   };
 
-  //const cvContent = form.watch("cvContent");
-
-  //useEffect(() => {
-  //  console.log(cvContent);
-  //}, [cvContent]);
-
-  const [cvInputType, setCvInputType] = useState<"upload" | "paste">("upload");
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="flex items-center justify-between gap-2">
-          Step 1: Upload your CV
-          <Button
-            variant="outline"
-            onClick={() =>
-              setCvInputType(cvInputType === "upload" ? "paste" : "upload")
-            }
-          >
-            {cvInputType === "upload"
-              ? "Paste your CV Text Instead"
-              : "Upload your CV File Instead"}
-          </Button>
-        </CardTitle>
-        <CardDescription>
-          {cvInputType === "upload"
-            ? "Upload your CV as a valid TXT, DOCX, or PDF file"
-            : "Paste your CV as text"}
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            {cvInputType === "upload" ? (
-              <> 123</>
-            ) : (
-              //<FormField
-              //  control={form.control}
-              //  name="cvContent"
-              //  render={({ field }) => (
-              //    <FormItem>
-              //      <FormLabel>CV Content</FormLabel>
-              //      <FileUpload
-              //        onContentChange={field.onChange}
-              //        value={field.value}
-              //      />
-              //    </FormItem>
-              //  )}
-              ///>
-              <FormField
-                control={form.control}
-                name="cvContent"
-                render={({ field, formState: { errors } }) => (
-                  <FormItem>
-                    <FormLabel>CV Content</FormLabel>
-                    <FormControl>
-                      <Textarea
-                        {...field}
-                        error={!!errors.cvContent}
-                        errorMessage={errors.cvContent?.message}
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-            )}
-            <Button type="submit">Analyze Job Post</Button>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <Card>
+          <CardHeader>
+            <CardTitle>Step 1: Paste your CV</CardTitle>
+            <CardDescription>
+              Paste your CV content as text in the field below
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <FormField
+              control={form.control}
+              name="cvContent"
+              render={({ field, formState: { errors } }) => (
+                <FormItem>
+                  <FormLabel>CV Content</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      {...field}
+                      error={!!errors.cvContent}
+                      errorMessage={errors.cvContent?.message}
+                      placeholder="Paste your CV content here..."
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Step 2: Paste your Job Description</CardTitle>
+            <CardDescription>
+              Paste your Job Description content as text in the field below
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <FormField
+              control={form.control}
+              name="jobDescription"
+              render={({ field, formState: { errors } }) => (
+                <FormItem>
+                  <FormLabel>Job Description</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      {...field}
+                      error={!!errors.jobDescription}
+                      errorMessage={errors.jobDescription?.message}
+                      placeholder="Paste your Job Description content here..."
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </CardContent>
+        </Card>
+        <div className="flex justify-center">
+          <Button type="submit">Analyze Job Post</Button>
+        </div>
+      </form>
+    </Form>
   );
 };
 
